7e05526eefdddb7bbc7a79b5f407bcb7e6287f59
Noah Wilson <47047865+nopwn@users.noreply.github.com>
2021-12-02 14:17:09 -0700

    Update main.py

--- 02/main.py
+++ 02/main.py
@@ -1,21 +1,33 @@
 from input import input
 
-def main():
+def calc(newInput, p2):
     horizontal = 0
     depth = 0
     aim = 0
-    newInput = []
-    for i in input.input:
-        newInput.append(i.split(" "))
     for i in range(len(newInput)):
         if newInput[i][0] == "forward":
             horizontal += int(newInput[i][1])
-            depth += (aim*int(newInput[i][1]))
+            if (p2==1):
+                depth += (aim*int(newInput[i][1]))
         elif newInput[i][0] == "down":
-            aim += int(newInput[i][1])
+            if (p2 == 1):
+                aim += int(newInput[i][1])
+            else:
+                depth += int(newInput[i][1])
         elif newInput[i][0] == "up":
-            aim -= int(newInput[i][1])
-    print(horizontal*depth)
+            if (p2 == 1):
+                aim -= int(newInput[i][1])
+            else:
+                depth-= int(newInput[i][1])
+    return(horizontal*depth)
+
+def main():
+    newInput = []
+    for i in input.input:
+        newInput.append(i.split(" "))
+    print("Part 1: " + str(calc(newInput, 0)))
+    print("Part 2: " + str(calc(newInput, 1)))
+
 
 if __name__ == "__main__":
     main()
